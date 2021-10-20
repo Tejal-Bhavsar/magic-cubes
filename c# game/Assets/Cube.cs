df2a9c0793d135423e604b86150090304114c754
Tejal Bhavsar <55701377+Tejal-Bhavsar@users.noreply.github.com>
2021-10-20 13:20:19 +0530

    adding debug script with log
    debug script with logs

---
+++ c# game/Assets/Cube.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello there magic cube");
+        Debug.Log("how you doing ....");
+        Debug.Log("ready for the stage show ?");
+
+        Debug.LogWarning("if you press the space nothing happenes");
+        Debug.LogError("if you smash keyboared nothing happese its currently not that active");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
